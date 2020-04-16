ae9afa1a82489ba8e497b3a245d421a0155e898c
Christian <nelson.christian.m@gmail.com>
2020-04-16 16:42:33 -0400

    basic html, bootstrap, and css for the homepage

---
+++ index.js
@@ -0,0 +1,19 @@
+const jumbotron = `
+<div class="container">
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4 cc">¢ommon ¢ents</h1>
+            <p class="lead">we help you find the best price across the web.</p>
+            <hr class="my-4">
+            <p>Go ahead and try a search.</p>
+            <p class="lead">
+            We support Best Buy, Amazon, Newegg and more.
+            </p>
+        </div>
+    </div>
+</div>
+`;
+
+const root = document.querySelector('#root');
+
+root.innerHTML = jumbotron;
