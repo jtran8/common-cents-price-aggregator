bda450cb957162d1c95f02065dc81de2c9523118
jtran8 <40408767+jtran8@users.noreply.github.com>
2020-05-01 15:18:13 -0400

    Create app.js

---
+++ Jacky/app.js
@@ -0,0 +1,33 @@
+// Require neccessary packages
+var express = require('express');
+var mysql = require('./dbcon.js');
+
+// Set up express
+var app = express();
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+
+// Set up paths
+app.use('/', require('./filters.js'));
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+ 
+// Set up error-handling
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
