53a1a248d0ab0edb348cc8e995431849bc899ca2
slusserad <40811342+slusserad@users.noreply.github.com>
2020-05-01 15:37:40 -0400

    Queries: Savings attribute added to results object array. Utils: functions to sort by price, alphabetical and calculate savings.

--- queries.js
+++ queries.js
@@ -65,21 +65,24 @@ const exampleResults = {
   results      : [
     {
       retailer : 'Best Buy',
-      price    : 701.0,
+      price: 701.0,
+      savings: 0.00,
       inStock  : true,
       url      :
         'https://www.bestbuy.com/site/iphone/iphone-11-shop-by-carrier/pcmcat1568147287090.c?id=pcmcat1568147287090'
     },
     {
       retailer : 'Apple',
-      price    : 699.99,
+      price: 699.99,
+      savings: 0.00,
       inStock  : true,
       url      :
         'https://www.apple.com/iphone-11/?afid=p238%7Cs2Eu8PK7l-dc_mtid_20925d2q39172_pcrid_431569481416_pgrid_80926719554_&cid=wwa-us-kwgo-iphone--slid--'
     },
     {
       retailer : 'Target',
-      price    : 710.0,
+      price: 710.0,
+      savings: 0.00,
       inStock  : false,
       url      : 'https://www.target.com/p/apple-iphone-11/-/A-78052843'
     }
