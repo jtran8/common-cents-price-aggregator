8fe1d61ce8dfcc77c3d0c2c3526ffa55b6ed4eea
jtran8 <40408767+jtran8@users.noreply.github.com>
2020-05-08 23:26:18 -0400

    Update filters.js

--- filters.js
+++ filters.js
@@ -4,7 +4,7 @@ module.exports = function(){
 
     function getCategories(res, mysql, context, complete){
         mysql.pool.query(
-            "SELECT catId, name FROM categories ORDER BY name ASC;",
+            "SELECT catId, name FROM categories ORDER BY name ASC",
             function(error, results, fields){
                if(error){
                     res.write(JSON.stringify(error));
@@ -31,7 +31,7 @@ module.exports = function(){
     }
 
     function getProducts(res, mysql, context, category, brand, complete){
-        var sql = "SELECT name, upc FROM products WHERE catId=? AND brandId=? ORDER BY name ASC;";
+        var sql = "SELECT name, upc FROM products WHERE catId=? AND brandId=? ORDER BY name ASC";
         var inserts = [category, brand];
 
         mysql.pool.query(sql, inserts, function(error, results, fields){
