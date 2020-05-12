cc2731d1b74a43c19e628968456ac6151845d431
ijprocel <ijprocel@gmail.com>
2020-05-12 14:45:53 -0500

    refactored upc_lookup.js to fix Bloater.Long_Method code smell

--- updater/upc_lookup.js
+++ updater/upc_lookup.js
@@ -12,12 +12,6 @@ mariadb.pool.query('SELECT upc FROM products', function(err, rows, fields){
   setInterval(updateItem, 865000);  //upcitemdb.com's free api only allows 100 calls per day. This is the proper interval to keep calls at that rate.
 });
 
-function insertUpdateProductRetailer(upc, sellerId, link, price){
-  mariadb.pool.query('INSERT INTO productRetailer (`upc`, `retailerId`, `price`, `url`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE price=?, url=?', [upc, sellerId, price, link, price, link], function(err, result){
-    if(err) throw err;
-  });
-}
-
 function updateItem(){
   if (idx == numProds){
     idx = 0
@@ -40,22 +34,8 @@ function updateItem(){
 
       res.on("end", () => {
         try {
-            let item_results = JSON.parse(response);
-            //console.log(response);
-            for (let i=0; i < item_results.items[0].offers.length; i++){
-              let seller = item_results.items[0].offers[i];
-              let sellerId;
-              mariadb.pool.query('INSERT INTO retailers (`name`) VALUES (?) ON DUPLICATE KEY UPDATE name=retailers.name', [seller.merchant, seller.merchant], function(err, result){
-                if(err) throw err;
+            AttemptProductUpdate(response, prods[idx].upc);
 
-                mariadb.pool.query('SELECT retailerId FROM retailers WHERE name=?', [seller.merchant], function(err, rows, fields){
-                  if(err) throw err;
-                  sellerId = rows[0].retailerId;
-                  insertUpdateProductRetailer(upc, rows[0].retailerId, seller.link, seller.price);
-                });
-              });
-            }
-            idx +=1;
         } catch (error) {
             console.error(error.message);
             idx +=1;
@@ -69,4 +49,34 @@ function updateItem(){
     
     req.end()
 }
-console.log("Running...");
+
+function AttemptProductUpdate(response, upc){
+  let item_results = JSON.parse(response);
+  for (let i=0; i < item_results.items[0].offers.length; i++){
+    let seller = item_results.items[0].offers[i];
+    AddRetailer(seller, upc);
+  }
+  idx +=1;
+}
+
+function AddRetailer(seller, upc){
+  mariadb.pool.query('INSERT INTO retailers (`name`) VALUES (?) ON DUPLICATE KEY UPDATE retailerId=retailerId', [seller.merchant], function(err, result){
+    if(err) throw err;
+    GetRetailerId(seller, upc)
+  });
+}
+
+function GetRetailerId(seller, upc){
+  mariadb.pool.query('SELECT retailerId FROM retailers WHERE name=?', [seller.merchant], function(err, rows, fields){
+    if(err) throw err;
+    insertUpdateProductRetailer(upc, rows[0].retailerId, seller.link, seller.price);
+  });
+}
+
+function insertUpdateProductRetailer(upc, sellerId, link, price){
+  mariadb.pool.query('INSERT INTO productRetailer (`upc`, `retailerId`, `price`, `url`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE price=?, url=?', [upc, sellerId, price, link, price, link], function(err, result){
+    if(err) throw err;
+  });
+}
+
+console.log("Update started...");
