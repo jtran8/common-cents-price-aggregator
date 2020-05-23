9667d279baef1f4464fd6cf3dc86062d2b5be2bf
Christian <nelson.christian.m@gmail.com>
2020-05-22 20:55:12 -0400

    fix bugs after refactor

--- dbcon.js
+++ dbcon.js
@@ -3,10 +3,11 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : '*******',
-  user            : '*******',
-  password        : '*******',
-  database        : '*******',
+  host            :
+    'price-aggregator-backend.coxajyvhvlmg.us-east-1.rds.amazonaws.com',
+  user            : 'front_end_query_user',
+  password        : 'standardmisplaceobjectivebishop',
+  database        : 'common_cents'
 });
 
 module.exports.pool = pool;
